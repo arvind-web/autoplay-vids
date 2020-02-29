6f43792b91b1ecefbac17147e3a25e855d508b10
Arvind Singh <arvind_singh@scalend.com>
2020-03-01 02:14:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const init = function(){
+$(document).ready(function() {
+
+    var media = $('video');
+    var tolerancePixel = 40;
+
+    function checkMedia(){
+        var scrollTop = $(window).scrollTop() + tolerancePixel;
+        var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+        media.each(function(index, el) {
+            var yTopMedia = $(this).offset().top;
+            var yBottomMedia = $(this).height() + yTopMedia;
+
+            if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ 
+                $(this).get(0).play();
+            } else {
+                $(this).get(0).pause();
+            }
+        });
+    }
+    $(document).on('scroll', checkMedia);
+
     cssScrollSnapPolyfill()
-}
-init();
+});
